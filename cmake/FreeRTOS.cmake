ebba140c693922fb455ecfc420cdee231780dcff
SIDER\i.dymov <smoke-lmt@mail.ru>
2025-10-07 14:09:33 +0300

    Пробую насторить игнорирование

---
+++ cmake/FreeRTOS.cmake
@@ -0,0 +1,9 @@
+
+
+include_directories(
+		    FreeRTOS/include	
+		    FreeRTOS/portable/MemMang
+		    FreeRTOS/portable/GCC/RISC-V
+		    FreeRTOS/portable/GCC/RISC-V/chip_specific_extensions/RV32I_PFIC_no_extensions
+		    FreeRTOS
+                    )
